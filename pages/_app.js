e7fd982d83746bb45b6045d290862fb750156fe3
Eloïse <eloise.richard@epitech.eu>
2021-11-26 23:37:05 +0100

    Added: Sidebar menu

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,12 @@
 import 'tailwindcss/tailwind.css'
 import React from "react";
 import { UserProvider } from "@auth0/nextjs-auth0";
-import Header from '../components/Header.js';
+import Navigation from '../components/Navigation.js';
 
 const App = ({ Component, pageProps }) => {
   return (
     <UserProvider>
-      {/* <Component {...pageProps} /> */}
-      <Header {...pageProps} />
+      <Navigation {...pageProps} />
     </UserProvider>
   );
 };
