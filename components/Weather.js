a708cdfbf84ab782753ebcbb4850f0b904937278
Eloïse <eloise.richard@epitech.eu>
2021-11-30 14:07:12 +0100

    Fix: Delete comment

--- components/Weather.js
+++ components/Weather.js
@@ -4,26 +4,26 @@ const Weather = () => {
   const [city, setCity] = useState("Nice");
 
   const addWidget = async (city, type) => {
-    if (type == "daily") {
-      const response = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.weatherKey}`
-      );
-      const data = await response.json();
-      console.log(data)
-    }
-    if (type == "weekly") {
-      let response = await fetch(
-        `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${process.env.weatherKey}`
-      );
-      let data = await response.json()
-      console.log(data)
-    }
-    // await fetch('/api/widgets/add', {
-    //     method: 'POST',
-    //     headers: { 'Content-Type': 'application/json' },
-    //     body: JSON.stringify({api_name: 'weather', auth_required: false, params: { city: city, type: type}})
-    // })
-    // window.location.reload();
+    // if (type == "daily") {
+    //   const response = await fetch(
+    //     `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.weatherKey}`
+    //   );
+    //   const data = await response.json();
+    //   console.log(data)
+    // }
+    // if (type == "weekly") {
+    //   let response = await fetch(
+    //     `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${process.env.weatherKey}`
+    //   );
+    //   let data = await response.json()
+    //   console.log(data)
+    // }
+    await fetch('/api/widgets/add', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({api_name: 'weather', auth_required: false, params: { city: city, type: type}})
+    })
+    window.location.reload();
   };
 
   return (
