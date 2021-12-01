c5a8bbffb2a4309b38c953b0d94f0b476064e98e
Alizee <alizee.patricola@epitech.eu>
2021-12-01 22:45:20 +0100

    widget weather daily ok

--- components/widgets/WWeather.js
+++ components/widgets/WWeather.js
@@ -5,8 +5,10 @@ import { useEffect, useState } from "react";
 const WWeather = (props) => {
   let currentParams = props.params.params;
   const [data, setData] = useState([]);
-  const [displayDataD, setDisplayDataD] = useState({ city: '', temp: '', weather: '', humidity: '', date: '', day: '' })
-  const [displayDataW, setDisplayDataW] = useState({ city: '', temp: '', weather: '', humidity: '', date: '', day: '' })
+  const [calcWeekly, setWeekly] = useState({});
+
+  const [displayDataD, setDisplayDataD] = useState({ icon: '', city: '', temp: '', weather: '', humidity: '', date: '', day: '' })
+  const [displayDataW, setDisplayDataW] = useState({ icon: '', city: '', temp: '', weather: '', humidity: '', date: '', day: '' })
 
   useEffect((type, city) => {
     const fetchWeather = async (type, city) => {
@@ -16,6 +18,7 @@ const WWeather = (props) => {
         );
         const data = await response.json();
         setData(data)
+        console.log(data)
       }
       if (type == "weekly") {
         let response = await fetch(
@@ -26,41 +29,45 @@ const WWeather = (props) => {
       }
     };
     fetchWeather(currentParams.type, currentParams.city);
-
     console.log(data)
     if (currentParams.type == 'daily' && (data.length != 0)) {
-      setDisplayDataD({ city: data.name, temp: data.main.temp, weather: data.weather[0].main, humidity: data.main.humidity, date: moment().format('dddd'), day: moment().format('LL') })
+      setDisplayDataD({ icon: data.weather[0].icon, city: data.name, temp: data.main.temp, weather: data.weather[0].main, humidity: data.main.humidity, date: moment().format('dddd'), day: moment().format('LL') })
     }
     if (currentParams.type == 'weekly' && (data.length != 0)) {
-      setDisplayDataW({ city: data.city.name, temp: data.list[1].main.temp, weather: data.list[1].weather[0].main, humidity: data.list[0].main.humidity, date: moment().format('dddd'), day: moment().format('LL') })
+      setDisplayDataW({ icon: data.list[1].weather[0].icon, city: data.city.name, temp: data.list[1].main.temp, weather: data.list[1].weather[0].main, humidity: data.list[0].main.humidity, date: moment().format('dddd'), day: moment().format('LL') })
+      setWeekly(data.list)
+      console.log(data.list)
     }
   }, []);
   return (
     <div class="container mx-auto">
       <div>
         {currentParams.type == "daily" && (
-          <div class="mt-2 flex flex-col justify-center bg-white w-full max-w-xs rounded-xl shadow p-4 ">
-            <div>
-            <div>day: {displayDataD.day}</div>
-            <div>date: {displayDataD.date}</div>
-            <div>ville: {displayDataD.city}</div>
-            <div>temperature: {displayDataD.temp} &deg;C</div>
-            <div>etat du ciel: {displayDataD.weather}</div>
-            <div>humidity: {displayDataD.humidity}%</div>
+          <div class="relative mt-2 flex flex-col justify-center bg-white w-full max-w-xs rounded-xl shadow-lg p-4 ">
+            <div>{displayDataD.date}, {displayDataD.day}</div>
+            <div class="font-family: -apple-system text-sm">{displayDataD.city}</div>
+            <div class="flex flex-col">
+              <img class="w-24 mx-auto"src={`http://openweathermap.org/img/w/${displayDataD.icon}.png`} />
+              <div class="h-6 text-xl">{Math.round(displayDataD.temp)}&deg;C</div>
             </div>
+            <div class="text-lg p-0">{displayDataD.weather}</div>
+            <div class="absolute bottom-2 right-2 font-family: -apple-system text-sm">humidity: {displayDataD.humidity}%</div>
           </div>
         )}
       </div>
       <div>
         {currentParams.type == "weekly" && (
-          <div class="mt-2 flex flex-col justify-center bg-white w-full max-w-xs rounded-xl shadow p-4 ">
-            <p>city: {displayDataW.city}</p>
-            <p>day: {displayDataW.day}</p>
-            <p>date: {displayDataW.date}</p>
-            <p>temp: {displayDataW.temp} &deg;C</p>
-            <p>weather: {displayDataW.weather}</p>
-            <p>humidity: {displayDataW.humidity}%</p>
-          </div>
+           <div class="relative mt-2 flex flex-col justify-center bg-white w-full max-w-xs rounded-xl shadow p-4 ">
+           <div>{displayDataW.date}, {displayDataW.day}</div>
+           <div class="font-family: -apple-system text-sm">{displayDataW.city}</div>
+           <div class="flex flex-col">
+             <img class="w-24 mx-auto"src={`http://openweathermap.org/img/w/${displayDataW.icon}.png`} />
+             <div class="h-6 text-xl">{Math.round(displayDataW.temp)} &deg;C</div>
+           </div>
+           {/* <div>{calcWeekly}</div> */}
+           <div>{displayDataW.weather}</div>
+           <div class="absolute bottom-2 right-2 font-family: -apple-system text-sm">humidity: {displayDataW.humidity}%</div>
+         </div>
         )}
       </div>
     </div>
