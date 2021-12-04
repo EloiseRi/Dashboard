0c29475bfd9cc5939124ee3d91a75c6d4291a465
Eloïse <eloise.richard@epitech.eu>
2021-12-04 19:10:36 +0100

    Add: Get of the host IP

--- pages/about.json.js
+++ pages/about.json.js
@@ -1,17 +1,15 @@
-export default function About({ data }) {
-  return(
-    <pre>
-      {JSON.stringify(data, undefined, 2)}
-    </pre>
-  )
+export default function About({ data, host }) {
+  return <pre>{JSON.stringify(data, undefined, 2)}</pre>;
 }
 
 export const getServerSideProps = async (context) => {
-  const res = await fetch('http://localhost:8080/api/about')
-  const data = await res.json()
+  const host = context.req.headers["host"];
+  const res = await fetch("http://localhost:8080/api/about");
+  const data = await res.json();
   return {
     props: {
-      data: data
-    }
-  }
-}
+      data: data,
+      host: host
+    },
+  };
+};
