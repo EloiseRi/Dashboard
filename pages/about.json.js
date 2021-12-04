77fbf3513c104c2312b97d769df64ee11c385c39
Alizee <alizee.patricola@epitech.eu>
2021-12-04 19:05:28 +0100

    adding about json

--- pages/about.json.js
+++ pages/about.json.js
@@ -1,8 +1,79 @@
 export default function About({ data }) {
-  return(
+  let data =
+  {
+    "client": {
+      "host": RemoteAddr
+    },
+    "server": {
+      "current_time": 1638640891,
+      "services": [{
+        "name": "weather",
+        "widgets": [{
+          "name": "Wheater",
+          "description": "Display temperature of the chosen city",
+          "params": [{
+            "name": "city",
+            "type": "string",
+          }, {
+            "name": "type",
+            "type": "string",
+          }]
+        }]
+      }, {
+        "name": "clock",
+        "widgets": [{
+          "name": "Clock",
+          "description": "Displaying the time of the chosen city",
+          "params": [{
+            "name": "continent",
+            "type": "string "
+          }, {
+            "name": "country",
+            "type": "string"
+          }]
+        }]
+      }, {
+        "name": "crypto",
+        "widgets": [{
+          "name": "crypto",
+          "description": "Displaying the informations of the chosen crypto",
+          "params": [{
+            "name": "pair",
+            "type": "string"
+          }, {
+            "name": "refrashData",
+            "type": "number"
+          }]
+        },]
+      }, {
+        "name": "movies",
+        "widgets": [{
+          "name": "Movies",
+          "description": "Displaying the popular, upcoming, top rated and in theaters now movies.",
+          "params": [{
+            "name": "type",
+            "type": "string"
+          }]
+        }]
+      }, {
+        "name": "spotify",
+        "widgets": [{
+          "name": "Spotify",
+          "description": "",
+          "params": [{
+            "name": "",
+            "type": ""
+          }]
+        }]
+      },
+      ]
+    }
+  };
+  return (
     <pre>
       {JSON.stringify(data, undefined, 2)}
     </pre>
+
   )
 }
 
