99c2933bced147b56838b56714ff98b742dcba4a
Eloïse <eloise.richard@epitech.eu>
2021-12-04 15:15:45 +0100

    Add: Front of movies widget

--- components/widgets/Movies.js
+++ components/widgets/Movies.js
@@ -1,5 +1,9 @@
 import { useEffect, useState, Fragment } from "react";
 import { Menu, Transition } from "@headlessui/react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 const useFindMovies = (props) => {
   const [type, setType] = useState(props);
@@ -56,10 +60,207 @@ const Movies = (props) => {
     fetchData();
   }, [type]);
 
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 464 },
+      items: 1,
+    },
+  };
   return (
-    <div>
-      <button onClick={handleClick}>DELETE BUTTON</button>
-      <h1>Movies</h1>
+    <div >
+      <div className="flex flex-col justify-center py-1 ">
+        <div className="relative max-w-xl mx-auto">
+          <div className="relative bg-white shadow-lg rounded-3xl pb-4 py-6 px-0 bg-clip-padding bg-opacity-60 border border-gray-100 w-72">
+            <div className="absolute top-2 right-1"> <button className="pr-2" onClick={handleClick}><FontAwesomeIcon className="h-3" icon={faTrashAlt}></FontAwesomeIcon></button></div>
+            <h1 className="text-2xl text-purple-800 divide-x divide-green-500">Movies</h1>
+            <div>
+              {data && type == "now_playing" && (
+                <Carousel
+                  swipeable={false}
+                  draggable={false}
+                  showDots={false}
+                  responsive={responsive}
+                  arrows={true}
+                  ssr={true} // means to render carousel on server-side.
+                  infinite={true}
+                  autoPlay={true}
+                  autoPlaySpeed={4000}
+                  keyBoardControl={true}
+                  customTransition="all .5"
+                  transitionDuration={500}
+                  partialVisibile={true}
+                  containerClass="carousel-container relative z-0"
+                  dotListClass="custom-dot-list-style"
+                  itemClass="carousel-item-padding-40-px"
+                >
+                  <div>
+                    <p className="text-sm mb-4">In Theaters Now</p>
+                    <p className="italic mb-1">{data.results[0].title}</p>
+                    <img className="h-32 mx-auto" src={`https://image.tmdb.org/t/p/original/${data.results[0].poster_path}`} />
+                    <p className="mt-4">{data.results[0].vote_average}/10</p>
+                  </div>
+                  <div>
+                    <p className="text-sm mb-4">In Theaters Now</p>
+                    <p className="italic mb-2">{data.results[2].title}</p>
+                    <img className="h-32 mx-auto" src={`https://image.tmdb.org/t/p/original/${data.results[2].poster_path}`} />
+                    <p className="mt-4">{data.results[2].vote_average}/10</p>
+                  </div>
+                  <div>
+                    <p className="text-sm mb-4">In Theaters Now</p>
+                    <p className="italic mb-2">{data.results[6].title}</p>
+                    <img className="h-32 mx-auto" src={`https://image.tmdb.org/t/p/original/${data.results[6].poster_path}`} />
+                    <p className="mt-4">{data.results[6].vote_average}/10</p>
+                  </div>
+                  <div>
+                    <p className="text-sm mb-4">In Theaters Now</p>
+                    <p className="italic mb-2">{data.results[9].title}</p>
+                    <img className="h-32 mx-auto" src={`https://image.tmdb.org/t/p/original/${data.results[9].poster_path}`} />
+                    <p className="mt-4">{data.results[9].vote_average}/10</p>
+                  </div>
+                </Carousel>
+              )}
+            </div>
+            <div>
+              {data && type == "popular" && (
+                <Carousel
+                  swipeable={false}
+                  draggable={false}
+                  showDots={false}
+                  responsive={responsive}
+                  arrows={true}
+                  ssr={false} // means to render carousel on server-side.
+                  infinite={true}
+                  autoPlay={true}
+                  autoPlaySpeed={5000}
+                  keyBoardControl={true}
+                  customTransition="all .5"
+                  transitionDuration={500}
+                  containerClass="carousel-container relative"
+                  dotListClass="custom-dot-list-style"
+                  itemClass="carousel-item-padding-10-px"
+                >
+                  <div>
+                    <p className="text-sm mb-4">Popular Movies</p>
+                    <p className="italic mb-1">{data.results[0].title}</p>
+                    <img className="h-32 mx-auto" src={`https://image.tmdb.org/t/p/original/${data.results[0].poster_path}`} />
+                    <p className="mt-4">{data.results[0].vote_average}/10</p>
+                  </div>
+                  <div>
+                    <p className="text-sm mb-4">Popular Movies</p>
+                    <p className="italic mb-2">{data.results[2].title}</p>
+                    <img className="h-32 mx-auto" src={`https://image.tmdb.org/t/p/original/${data.results[2].poster_path}`} />
+                    <p className="mt-4">{data.results[2].vote_average}/10</p>
+                  </div>
+                  <div>
+                    <p className="text-sm mb-4">Popular Movies</p>
+                    <p className="italic mb-2">{data.results[6].title}</p>
+                    <img className="h-32 mx-auto" src={`https://image.tmdb.org/t/p/original/${data.results[6].poster_path}`} />
+                    <p className="mt-4">{data.results[6].vote_average}/10</p>
+                  </div>
+                  <div>
+                    <p className="text-sm mb-4">Popular Movies</p>
+                    <p className="italic mb-2">{data.results[9].title}</p>
+                    <img className="h-32 mx-auto" src={`https://image.tmdb.org/t/p/original/${data.results[9].poster_path}`} />
+                    <p className="mt-4">{data.results[9].vote_average}/10</p>
+                  </div>
+                </Carousel>
+              )}
+            </div>
+            <div>
+              {data && type == "top_rated" && (
+                <Carousel
+                  swipeable={false}
+                  draggable={false}
+                  showDots={false}
+                  responsive={responsive}
+                  arrows={true}
+                  ssr={false} // means to render carousel on server-side.
+                  infinite={true}
+                  autoPlay={true}
+                  autoPlaySpeed={5000}
+                  keyBoardControl={true}
+                  customTransition="all .5"
+                  transitionDuration={500}
+                  containerClass="carousel-container relative"
+                  dotListClass="custom-dot-list-style"
+                  itemClass="carousel-item-padding-10-px"
+                >
+                  <div>
+                    <p className="text-sm mb-4">Top Rated Movies</p>
+                    <p className="italic mb-1">{data.results[0].title}</p>
+                    <img className="h-32 mx-auto" src={`https://image.tmdb.org/t/p/original/${data.results[0].poster_path}`} />
+                    <p className="mt-4">{data.results[0].vote_average}/10</p>
+                  </div>
+                  <div>
+                    <p className="text-sm mb-4">Top Rated Movies</p>
+                    <p className="italic mb-2">{data.results[2].title}</p>
+                    <img className="h-32 mx-auto" src={`https://image.tmdb.org/t/p/original/${data.results[2].poster_path}`} />
+                    <p className="mt-4">{data.results[2].vote_average}/10</p>
+                  </div>
+                  <div>
+                    <p className="text-sm mb-4">Top Rated Movies</p>
+                    <p className="italic mb-2">{data.results[6].title}</p>
+                    <img className="h-32 mx-auto" src={`https://image.tmdb.org/t/p/original/${data.results[6].poster_path}`} />
+                    <p className="mt-4">{data.results[6].vote_average}/10</p>
+                  </div>
+                  <div>
+                    <p className="text-sm mb-4">Top Rated Movies</p>
+                    <p className="italic mb-2">{data.results[9].title}</p>
+                    <img className="h-32 mx-auto" src={`https://image.tmdb.org/t/p/original/${data.results[9].poster_path}`} />
+                    <p className="mt-4">{data.results[9].vote_average}/10</p>
+                  </div>
+                </Carousel>
+              )}
+            </div>
+            <div>
+              {data && type == "upcoming" && (
+                <Carousel
+                  swipeable={false}
+                  draggable={false}
+                  showDots={false}
+                  responsive={responsive}
+                  arrows={true}
+                  ssr={false} // means to render carousel on server-side.
+                  infinite={true}
+                  autoPlay={true}
+                  autoPlaySpeed={5000}
+                  keyBoardControl={true}
+                  customTransition="all .5"
+                  transitionDuration={500}
+                  containerClass="carousel-container relative"
+                  dotListClass="custom-dot-list-style"
+                  itemClass="carousel-item-padding-10-px"
+                >
+                  <div>
+                    <p className="text-sm mb-4">Upcoming Movies</p>
+                    <p className="italic mb-1">{data.results[0].title}</p>
+                    <img className="h-32 mx-auto" src={`https://image.tmdb.org/t/p/original/${data.results[0].poster_path}`} />
+                    <p className="mt-4">{data.results[0].vote_average}/10</p>
+                  </div>
+                  <div>
+                    <p className="text-sm mb-4">Upcoming Movies</p>
+                    <p className="italic mb-2">{data.results[2].title}</p>
+                    <img className="h-32 mx-auto" src={`https://image.tmdb.org/t/p/original/${data.results[2].poster_path}`} />
+                    <p className="mt-4">{data.results[2].vote_average}/10</p>
+                  </div>
+                  <div>
+                    <p className="text-sm mb-4">Upcoming Movies</p>
+                    <p className="italic mb-2">{data.results[6].title}</p>
+                    <img className="h-32 mx-auto" src={`https://image.tmdb.org/t/p/original/${data.results[6].poster_path}`} />
+                    <p className="mt-4">{data.results[6].vote_average}/10</p>
+                  </div>
+                  <div>
+                    <p className="text-sm mb-4">Upcoming Movies</p>
+                    <p className="italic mb-2">{data.results[9].title}</p>
+                    <img className="h-32 mx-auto" src={`https://image.tmdb.org/t/p/original/${data.results[9].poster_path}`} />
+                    <p className="mt-4">{data.results[9].vote_average}/10</p>
+                  </div>
+                </Carousel>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="w-56 fixed top-20 text-right">
         <Menu as="div" className="relative inline-block text-left">
           <div>
@@ -81,9 +282,8 @@ const Movies = (props) => {
                 <Menu.Item>
                   {({ active }) => (
                     <button
-                      className={`${
-                        active ? "bg-violet-500 text-white" : "text-gray-900"
-                      } group flex rounded-md items-center w-full px-2 py-2 text-sm`}
+                      className={`${active ? "bg-violet-500 text-white" : "text-gray-900"
+                        } group flex rounded-md items-center w-full px-2 py-2 text-sm`}
                       value={"popular"}
                       onClick={handleChange}
                     >
@@ -94,9 +294,8 @@ const Movies = (props) => {
                 <Menu.Item>
                   {({ active }) => (
                     <button
-                      className={`${
-                        active ? "bg-violet-500 text-white" : "text-gray-900"
-                      } group flex rounded-md items-center w-full px-2 py-2 text-sm`}
+                      className={`${active ? "bg-violet-500 text-white" : "text-gray-900"
+                        } group flex rounded-md items-center w-full px-2 py-2 text-sm`}
                       value={"top_rated"}
                       onClick={handleChange}
                     >
@@ -107,9 +306,8 @@ const Movies = (props) => {
                 <Menu.Item>
                   {({ active }) => (
                     <button
-                      className={`${
-                        active ? "bg-violet-500 text-white" : "text-gray-900"
-                      } group flex rounded-md items-center w-full px-2 py-2 text-sm`}
+                      className={`${active ? "bg-violet-500 text-white" : "text-gray-900"
+                        } group flex rounded-md items-center w-full px-2 py-2 text-sm`}
                       value={"now_playing"}
                       onClick={handleChange}
                     >
@@ -120,9 +318,8 @@ const Movies = (props) => {
                 <Menu.Item>
                   {({ active }) => (
                     <button
-                      className={`${
-                        active ? "bg-violet-500 text-white" : "text-gray-900"
-                      } group flex rounded-md items-center w-full px-2 py-2 text-sm`}
+                      className={`${active ? "bg-violet-500 text-white" : "text-gray-900"
+                        } group flex rounded-md items-center w-full px-2 py-2 text-sm`}
                       value={"upcoming"}
                       onClick={handleChange}
                     >
